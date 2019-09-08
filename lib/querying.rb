cddaa4795150ed177fad08dc1bc81bfad5dee39d
github username <github email address>
2019-09-08 18:09:34 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series LEFT JOIN characters ON characters.series_id = series.id LIMIT 1;"
+  "SELECT series.title FROM series INNER JOIN books ON books.series_id = series.id INNER JOIN character_books ON character_books.book_id = books.id INNER JOIN characters ON character_books.character_id = characters.id WHERE characters.species = 'human' GROUP BY series.title ORDER by COUNT(*) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
